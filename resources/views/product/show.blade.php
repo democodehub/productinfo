a4f8fa522bd5bc161f3b3c6bdde1c393664cbc21
mixmaster929 <smart.topdev929@gmail.com>
2024-07-23 17:21:29 +0200

    added logo into detail page

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -19,25 +19,30 @@
         <!-- Display product details -->
         <div class="product-details">
             @if ($product)
-                <p><strong>Product ID:</strong> {{ $product->id }}</p>
-                <p><strong>Product Code:</strong> {{ $product->product_code }}</p>
-                <p><strong>Product Name:</strong> {{ $product->product_name }}</p>
-                <p><strong>Length:</strong> {{ $product->length }}</p>
-                <p><strong>Height:</strong> {{ $product->height }}</p>
-                <p><strong>Depth:</strong> {{ $product->depth }}</p>
-                <p><strong>Customer Name:</strong> {{ $product->customer_name }}</p>
-                <p><strong>Project Name:</strong> {{ $product->project_name }}</p>
-                <p><strong>Address:</strong> {{ $product->address }}</p>
-                <p><strong>Production Date:</strong> {{ $formattedProductionDate }}</p>
-                <p><strong>Delivery Date:</strong> {{ $formattedDeliveryDate }}</p>
-                <p><strong>Description:</strong> {{ $product->description }}</p>
-            @else
-                <p><strong>There is no product</strong></p>
+                <div>
+                    <img src="{{ asset('images/logo.png') }}" class="logo" alt="Description of image">
+                </div>
+                <div>
+                    <p><strong>Product ID:</strong> {{ $product->id }}</p>
+                    <p><strong>Product Code:</strong> {{ $product->product_code }}</p>
+                    <p><strong>Product Name:</strong> {{ $product->product_name }}</p>
+                    <p><strong>Length:</strong> {{ $product->length }}</p>
+                    <p><strong>Height:</strong> {{ $product->height }}</p>
+                    <p><strong>Depth:</strong> {{ $product->depth }}</p>
+                    <p><strong>Customer Name:</strong> {{ $product->customer_name }}</p>
+                    <p><strong>Project Name:</strong> {{ $product->project_name }}</p>
+                    <p><strong>Address:</strong> {{ $product->address }}</p>
+                    <p><strong>Production Date:</strong> {{ $formattedProductionDate }}</p>
+                    <p><strong>Delivery Date:</strong> {{ $formattedDeliveryDate }}</p>
+                    <p><strong>Description:</strong> {{ $product->description }}</p>
+                @else
+                    <p><strong>There is no product</strong></p>
             @endif
         </div>
+    </div>
 
-        <!-- Back to product list button -->
-        <a href="#" class="btn btn-primary">Back to Product List</a>
+    <!-- Back to product list button -->
+    <a href="#" class="btn btn-primary">Back to Product List</a>
     </div>
 </body>
 
